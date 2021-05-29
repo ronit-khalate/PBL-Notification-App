e41a1140f093a3dec44655eeac37854755945c01
Prasadthegreat <prasadthegreat799@gmail.com>
2021-05-29 18:28:49 +0530

    "Added Splash screen"

---
+++ app/src/main/java/com/prasadthegreat/reminderapp/Model.java
@@ -0,0 +1,39 @@
+package com.prasadthegreat.reminderapp;
+
+public class Model
+{
+    String title,date,time;
+
+    public Model() {}
+
+    public Model(String title, String date, String time)
+    {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
